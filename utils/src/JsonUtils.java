afc46521ac56c8e0712c3c7e201f566cc04b232c
Meng-Ze Chen <mchen149@asu.edu>
2019-04-02 15:08:49 -0700

    US#7
    Use alternative library (gson) to handle json file

--- utils/src/JsonUtils.java
+++ utils/src/JsonUtils.java
@@ -6,9 +6,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import org.json.simple.parser.JSONParser;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+
 
 public class JsonUtils {
     public static String getStrFromFile(String absoluteFilename) {
@@ -27,23 +30,23 @@ public class JsonUtils {
     }
 
     public static List<Question> getQuestionsFromJsonString(String jsonString) {
-        JSONParser jsonParser = new JSONParser();
-        JSONObject jsonContentObject = new JSONObject();
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jsonContentObject = new JsonObject();
         ArrayList<Question> questions = new ArrayList<Question>();
         try {
-            jsonContentObject = (JSONObject) jsonParser.parse(jsonString);
-            JSONArray contentArray = (JSONArray) jsonContentObject.get(ConstantTable.QUIZ_IDENTIFIER);
+            jsonContentObject = jsonParser.parse(jsonString).getAsJsonObject();
+            JsonArray contentArray = jsonContentObject.get(ConstantTable.QUIZ_IDENTIFIER).getAsJsonArray();
 
-            for (Object obj : contentArray) {
-                JSONObject questionJsonObj = (JSONObject) obj;
-                JSONArray optionArrays = (JSONArray) questionJsonObj.get(ConstantTable.QUESTION_OPTIONS);
+            for (JsonElement questionElem : contentArray) {
+                JsonObject questionJsonObj = questionElem.getAsJsonObject();
+                JsonArray optionsArray = (JsonArray) questionJsonObj.get(ConstantTable.QUESTION_OPTIONS).getAsJsonArray();
 
-                String title = (String) questionJsonObj.get(ConstantTable.QUESTION_TITLE);
+                String title = (String) questionJsonObj.get(ConstantTable.QUESTION_TITLE).getAsString();
                 ArrayList<String> optionsInThisQuestion = new ArrayList<String>();
-                String correctAnswer = (String) questionJsonObj.get(ConstantTable.QUESTION_CORRECT_ANSWER);
+                String correctAnswer = (String) questionJsonObj.get(ConstantTable.QUESTION_CORRECT_ANSWER).getAsString();
 
-                for (Object optionObj: optionArrays) {
-                    String optionStr = (String) optionObj;
+                for (JsonElement elem: optionsArray) {
+                    String optionStr = (String) elem.getAsString();
                     optionsInThisQuestion.add(optionStr);
                 }
 
@@ -63,9 +66,33 @@ public class JsonUtils {
         return null;
     }
 
+    public static String getQuestionsJsonString(List<Question> questions) {
+        JsonObject resultObject = new JsonObject();
+        JsonArray allQuestions = new JsonArray();
+
+        for (Question question: questions) {
+            JsonObject questionJsonObj = new JsonObject();
+            questionJsonObj.add(ConstantTable.QUESTION_TITLE, new JsonPrimitive(question.getTitle()));
+            questionJsonObj.add(ConstantTable.QUESTION_CORRECT_ANSWER, new JsonPrimitive(question.getCorrectAnswer()));
+            JsonArray optionsArray = new JsonArray();
+            for (String optionStatement: question.getOptions()) {
+                optionsArray.add(new JsonPrimitive(optionStatement));
+            }
+            questionJsonObj.add(ConstantTable.QUESTION_OPTIONS, optionsArray);
+            allQuestions.add(questionJsonObj);
+        }
+
+        resultObject.add(ConstantTable.QUIZ_IDENTIFIER, allQuestions);
+
+        return resultObject.toString();
+    }
+
     public static void main(String[] args) {
         String jsonString = getStrFromFile("/Users/meng/test.json");
+        System.out.println(jsonString);
         List<Question> questions = getQuestionsFromJsonString(jsonString);
-        System.out.println(questions.get(0).getCorrectAnswer());
+        String jsonStringResult = getQuestionsJsonString(questions);
+
+        System.out.println(jsonStringResult);
     }
 }
