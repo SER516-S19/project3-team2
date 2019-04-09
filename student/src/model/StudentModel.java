d90765b27f4b8f85cee527a9f683119adf5a53ec
Subhradeep Biswas <sbiswa24@asu.edu>
2019-04-09 00:37:17 -0700

    US#3: Retrieving questions from json file for a selected quiz

--- student/src/model/StudentModel.java
+++ student/src/model/StudentModel.java
@@ -1,6 +1,23 @@
-package src.model;
+/* 
+Student model to read the .json file and create question list
+@author Subhradeep 
+@version 1.1
+@date 04/09/2019
+*/
 
+package student.model;
+
+import utility.utils.*;
+import java.util.ArrayList;
 
 public class StudentModel {
-
+	private ArrayList<Question> questionList;
+	
+	public ArrayList<Question> getQuestionList(String str) {
+		String filePath = "C:/Users/subhr/Desktop/Desktop_Folders/SER516/QuizCreation/";
+		String jsonStrAllQst =  JsonUtils.getStringFromFile(filePath+str+".json");
+		
+		this.questionList = new ArrayList<Question>(JsonUtils.getQuestionsFromJsonString(jsonStrAllQst));
+		return this.questionList;
+	}
 }
