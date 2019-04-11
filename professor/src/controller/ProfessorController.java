4c45a896f40fb545b6eaff09b664e38ce33bd66e
Lehar Bhatt <bhattlehar@gmail.com>
2019-04-10 18:03:31 -0700

    US5-Task-27 - Integerate the frontend with backend to create the quiz based on UI input

--- professor/src/controller/ProfessorController.java
+++ professor/src/controller/ProfessorController.java
@@ -2,14 +2,10 @@ package src.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import utils.*;
 
-//import com.fasterxml.jackson.databind.ObjectMapper;
+import src.model.Constants;
+import utils.*;
 
 /**
  * 
@@ -18,73 +14,22 @@ import utils.*;
  */
 
 public class ProfessorController implements ActionListener {
+	private String actionType;
+	private List<Question> questionList;
+	private static final String absolutePath = System.getProperty("user.dir")+"/quiz/quiz1.txt";
+	
+	public ProfessorController(String actionType, List<Question> questionList){
+		super();
+		this.actionType = actionType;
+		this.questionList = questionList;
+	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent event) {
+		if(Constants.CREATE_QUIZ.equals(this.actionType)) {
+			String questionListString = JsonUtils.getJsonStringFromQuestions(questionList);
+			JsonUtils.writeStringToFile(absolutePath, questionListString);
+		}
 		
 	}
-
-	/*String classpath = ProfessorController.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-	final String JSONEXTENSION = ".json";
-	private List<Question> questions = new ArrayList<Question>();
-	private String quizName;
-
-	public ProfessorController(List<Question> questions, String quizName) {
-		this.questions.addAll(questions);
-		this.quizName = quizName;
-	}
-
-	public ProfessorController() {
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		System.out.print(classpath);
-		try {
-			List<Question> questionsList = new ArrayList<Question>();
-			List<String> options1 = Arrays.asList("Option 1", "Option 2", "Option 3", "Option 4");
-			Question q1 = new Question("Question1", options1, "Option 1");
-			List<String> options2 = Arrays.asList("Option 1", "Option 2", "Option 3", "Option 4");
-			Question q2 = new Question("Question2", options2, "Option 4");
-
-			questionsList.add(q1);
-			questionsList.add(q2);
-			//ObjectMapper mapper = new ObjectMapper();
-			mapper.writerWithDefaultPrettyPrinter()
-					.writeValue(new File(classpath + File.separator + quizName + JSONEXTENSION), questionsList);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-	}
-	/**
-	 * This is a test method. Delete after the UI integration is complete
-	 */
-	/*public void testMethod() {
-		// File oneMoreDirectory = new File(classpath + File.separator + "file1.json");
-		List<Question> questionsList = new ArrayList<Question>();
-		List<String> options1 = Arrays.asList("Option 1", "Option 2", "Option 3", "Option 4");
-		Question q1 = new Question("Question1", options1, "Option 1");
-		List<String> options2 = Arrays.asList("Option 1", "Option 2", "Option 3", "Option 4");
-		Question q2 = new Question("Question2", options2, "Option 4");
-
-		questionsList.add(q1);
-		questionsList.add(q2);
-		ObjectMapper mapper = new ObjectMapper();
-
-		try {
-
-			// Writing to a file
-			mapper.writerWithDefaultPrettyPrinter().writeValue(new File(classpath + File.separator + "quiz2.json"),
-					questionsList);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String args[]) {
-		ProfessorController con = new ProfessorController();
-		con.testMethod();
-	}*/
 }
--- professor/src/view/AddQuestions.java
+++ professor/src/view/AddQuestions.java
@@ -23,6 +23,9 @@ import java.awt.event.MouseMotionAdapter;
 import java.awt.Toolkit;
 import javax.swing.border.BevelBorder;
 
+import src.controller.ProfessorController;
+import src.model.Constants;
+
 public class AddQuestions extends JFrame {
 
 	protected static final String MouseEvent = null;
@@ -32,9 +35,9 @@ public class AddQuestions extends JFrame {
 	private JTextField answerField;
 	private JTextField[] optionField;
 	int x1, y1;
-	
+
 	// this is the list which has questions..
-	//ProfMainWindow professorWindow = new ProfMainWindow();
+	// ProfMainWindow professorWindow = new ProfMainWindow();
 	private static ArrayList<Question> questionsList = new ArrayList<Question>();
 
 	/**
@@ -49,7 +52,7 @@ public class AddQuestions extends JFrame {
 					Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 					int screenHeight = screenSize.height;
 					int screenWidth = screenSize.width;
-					frame.setSize((7*screenWidth) / 15, (8*screenHeight) / 9);
+					frame.setSize((7 * screenWidth) / 15, (8 * screenHeight) / 9);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -75,42 +78,42 @@ public class AddQuestions extends JFrame {
 		contentPane.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblAddQuestionDetails = new JLabel("Add Question Details Here");
 		lblAddQuestionDetails.setBounds(144, 112, 607, 80);
 		lblAddQuestionDetails.setFont(new Font("Courier", Font.BOLD, 40));
 		lblAddQuestionDetails.setForeground(new Color(255, 255, 255));
 		contentPane.add(lblAddQuestionDetails);
-		
+
 		JLabel lblOption_1 = new JLabel("Option1");
 		lblOption_1.setBounds(144, 327, 125, 38);
 		lblOption_1.setForeground(new Color(255, 255, 255));
 		lblOption_1.setFont(new Font("Monospaced", Font.BOLD, 25));
 		contentPane.add(lblOption_1);
-		
+
 		questionField = new JTextField();
 		questionField.setBounds(393, 252, 358, 35);
 		contentPane.add(lblOption_1);
-		
+
 		questionField_1 = new JTextField();
 		questionField_1.setBounds(393, 250, 358, 35);
 		contentPane.add(questionField_1);
 		questionField_1.setColumns(10);
-		
+
 		optionField = new JTextField[4];
-		
-		for(int i =0;i<4;i++) {
+
+		for (int i = 0; i < 4; i++) {
 			optionField[i] = new JTextField();
-			optionField[i].setBounds(393, 330+(i*81), 358, 38);
+			optionField[i].setBounds(393, 330 + (i * 81), 358, 38);
 			contentPane.add(optionField[i]);
 			optionField[i].setColumns(10);
 		}
-		
+
 		answerField = new JTextField();
 		answerField.setBounds(393, 650, 358, 38);
 		contentPane.add(answerField);
 		answerField.setColumns(10);
-		
+
 		JButton btnAddMoreQuestions = new JButton("Add Question");
 		btnAddMoreQuestions.setBounds(144, 742, 305, 49);
 		btnAddMoreQuestions.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
@@ -119,25 +122,26 @@ public class AddQuestions extends JFrame {
 		btnAddMoreQuestions.setBackground(new Color(0, 181, 204));
 		btnAddMoreQuestions.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				String strQuestionField = questionField.getText();
 				List<String> optionsList = new ArrayList<String>();
-				
-				for(int i =0;i<4;i++) {
+
+				for (int i = 0; i < 4; i++) {
 					optionsList.add(optionField[i].getText());
 				}
 				String strAnswerField = answerField.getText();
-			
-				Question q1 =  new Question(strQuestionField, optionsList, strAnswerField);
-				
+
+				Question q1 = new Question(strQuestionField, optionsList, strAnswerField);
+
 				questionsList.add(q1);
 				dispose();
-			    new AddQuestions().setVisible(true);
-				JOptionPane.showMessageDialog(null,"Question has been successfully Added!","Message", JOptionPane.INFORMATION_MESSAGE);
+				new AddQuestions().setVisible(true);
+				JOptionPane.showMessageDialog(null, "Question has been successfully Added!", "Message",
+						JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
 		contentPane.add(btnAddMoreQuestions);
-		
+
 		JButton btnDeleteQuestions = new JButton("Delete Question");
 		btnDeleteQuestions.setBounds(459, 742, 292, 49);
 		btnDeleteQuestions.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
@@ -145,86 +149,88 @@ public class AddQuestions extends JFrame {
 		btnDeleteQuestions.setBackground(new Color(0, 181, 204));
 		btnDeleteQuestions.setForeground(new Color(255, 255, 255));
 		btnDeleteQuestions.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {	
+			public void actionPerformed(ActionEvent arg0) {
 				String strQuestionField = questionField.getText();
-				
-				if (questionsList != null) {	
+
+				if (questionsList != null) {
 					Iterator<Question> iter = questionsList.iterator();
 
 					while (iter.hasNext()) {
 						Question q = iter.next();
-					    if (q.getTitle().equalsIgnoreCase(strQuestionField)) {
-					    	iter.remove();
-					    }
+						if (q.getTitle().equalsIgnoreCase(strQuestionField)) {
+							iter.remove();
+						}
 					}
-					
-				}else {
-					JOptionPane.showMessageDialog(null,"You cannot delete as there are no questions left to delete","Delete Message", JOptionPane.INFORMATION_MESSAGE);
+
+				} else {
+					JOptionPane.showMessageDialog(null, "You cannot delete as there are no questions left to delete",
+							"Delete Message", JOptionPane.INFORMATION_MESSAGE);
 				}
-		
+
 			}
 		});
 		contentPane.add(btnDeleteQuestions);
-		
+
 		JLabel label = new JLabel("");
 		label.setBounds(509, 704, 46, 14);
 		contentPane.add(label);
-		
+
 		JLabel lblOption = new JLabel("Option2");
 		lblOption.setBounds(144, 408, 125, 38);
 		lblOption.setForeground(new Color(255, 255, 255));
 		lblOption.setFont(new Font("Monospaced", Font.BOLD, 25));
 		contentPane.add(lblOption);
-		
+
 		JLabel lblQuestion = new JLabel("Question");
 		lblQuestion.setBounds(144, 247, 125, 38);
 		lblQuestion.setForeground(new Color(255, 255, 255));
 		lblQuestion.setFont(new Font("Monospaced", Font.BOLD, 25));
 		contentPane.add(lblQuestion);
-		
+
 		JLabel lblOption_2 = new JLabel("Option3");
 		lblOption_2.setBounds(144, 486, 125, 38);
 		lblOption_2.setForeground(new Color(255, 255, 255));
 		lblOption_2.setFont(new Font("Monospaced", Font.BOLD, 25));
 		contentPane.add(lblOption_2);
-		
+
 		JLabel lblOption_3 = new JLabel("Option4");
 		lblOption_3.setBounds(144, 569, 125, 38);
 		lblOption_3.setForeground(new Color(255, 255, 255));
 		lblOption_3.setFont(new Font("Monospaced", Font.BOLD, 25));
 		contentPane.add(lblOption_3);
-		
+
 		JLabel lblCorrectAnswer = new JLabel("Correct Answer");
 		lblCorrectAnswer.setBounds(144, 649, 210, 38);
 		lblCorrectAnswer.setForeground(new Color(255, 255, 255));
 		lblCorrectAnswer.setFont(new Font("Monospaced", Font.BOLD, 25));
 		contentPane.add(lblCorrectAnswer);
-		
+
 		JButton btnCreateQuizAnd = new JButton("Create Quiz And Exit");
 		btnCreateQuizAnd.setBounds(144, 807, 607, 49);
-		btnCreateQuizAnd.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				String strQuestionField = questionField.getText();
-				List<String> optionsList = new ArrayList<String>();
-				String strAnswerField = answerField.getText();
-				for(int i =0;i<4;i++) {
-					optionsList.add(optionField[i].getText());
-				}
-			
-				Question q1 =  new Question(strQuestionField, optionsList, strAnswerField);
-				questionsList.add(q1);
-				JOptionPane.showMessageDialog(null,"Quiz has been successfully Created!","Message", JOptionPane.INFORMATION_MESSAGE);
-				dispose();
-			}
-		});
+//		btnCreateQuizAnd.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//
+//				String strQuestionField = questionField.getText();
+//				List<String> optionsList = new ArrayList<String>();
+//				String strAnswerField = answerField.getText();
+//				for (int i = 0; i < 4; i++) {
+//					optionsList.add(optionField[i].getText());
+//				}
+//
+//				Question q1 = new Question(strQuestionField, optionsList, strAnswerField);
+//				questionsList.add(q1);
+//				JOptionPane.showMessageDialog(null, "Quiz has been successfully Created!", "Message",
+//						JOptionPane.INFORMATION_MESSAGE);
+//				dispose();
+//			}
+//		});
+		btnCreateQuizAnd.addActionListener(new ProfessorController(Constants.CREATE_QUIZ, questionsList));
 		btnCreateQuizAnd.setForeground(Color.WHITE);
 		btnCreateQuizAnd.setFont(new Font("Monospaced", Font.BOLD, 24));
 		btnCreateQuizAnd.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
 		btnCreateQuizAnd.setBackground(new Color(0, 181, 204));
 		contentPane.add(btnCreateQuizAnd);
-		
-	
+
 		JPanel panel2 = new JPanel();
 		panel2.setBounds(0, 0, 900, 43);
 		panel2.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
@@ -236,18 +242,18 @@ public class AddQuestions extends JFrame {
 				y1 = e.getY();
 			}
 		});
-	
+
 		panel2.addMouseMotionListener(new MouseMotionAdapter() {
 			@Override
 			public void mouseDragged(MouseEvent e) {
 				int xx1 = e.getXOnScreen();
 				int yy1 = e.getYOnScreen();
-				setLocation(xx1-x1, yy1-y1);
+				setLocation(xx1 - x1, yy1 - y1);
 			}
 		});
 		contentPane.add(panel2);
 		panel2.setLayout(null);
-	
+
 		JLabel lblNewLabel_2 = new JLabel("X");
 		lblNewLabel_2.setForeground(Color.WHITE);
 		lblNewLabel_2.addMouseListener(new MouseAdapter() {
@@ -256,12 +262,12 @@ public class AddQuestions extends JFrame {
 				dispose();
 			}
 		});
-	
+
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 30));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2.setBounds(846, 0, 54, 43);
 		panel2.add(lblNewLabel_2);
-	
+
 		JLabel lblNewLabel2 = new JLabel("  Quiz Desktop Application - Professor Window");
 		lblNewLabel2.setFont(new Font("Monospaced", Font.BOLD, 16));
 		lblNewLabel2.setForeground(Color.WHITE);
