503168eb8262faec4c13c6acfbc712e09368d796
Lehar Bhatt <bhattlehar@gmail.com>
2019-04-11 22:17:35 -0700

    Create folder to store quiz

--- professor/src/controller/ProfessorController.java
+++ professor/src/controller/ProfessorController.java
@@ -2,6 +2,7 @@ package src.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.ArrayList;
 
 import src.*;
@@ -15,7 +16,7 @@ import src.*;
 public class ProfessorController implements ActionListener {
 	private String actionType;
 	private ArrayList<Question> questionList;
-	private static final String absolutePath = System.getProperty("user.dir")+"/quiz/quiz1.txt";
+	private static final String quizPath = System.getProperty("user.home")+"/quizabc/";
 	
 	public ProfessorController(String actionType, ArrayList<Question> questionList){
 		super();
@@ -25,6 +26,16 @@ public class ProfessorController implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
+		File directory = new File(quizPath);
+	    boolean exists = directory.exists();
+	    String absolutePath;
+	    if(!exists){
+	    	boolean file = new File(quizPath).mkdir();
+	    }
+	    
+	    absolutePath = quizPath + "/BJP.json";
+	    
+	    
 		if (this.actionType.equals(ConstantTable.CONTROLER_IDENTIFIER_CREATE_QUIZ)) {
 			System.out.println(this.actionType);
 
